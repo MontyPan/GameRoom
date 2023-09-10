2957bdb586676b6f2b87e27d6b0665bc2d58b1de
MontyPan <monty.pan@dontcareabout.us>
2023-09-10 16:42:11 +0800

    增加：ImageRS 以及對應圖檔

---
+++ src/main/java/us/dontcareabout/gameRoom/client/mine/ImageRS.java
@@ -0,0 +1,22 @@
+package us.dontcareabout.gameRoom.client.mine;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+interface ImageRS extends ClientBundle {
+	static ImageRS I = GWT.create(ImageRS.class);
+
+	ImageResource b0();
+	ImageResource b1();
+	ImageResource b2();
+	ImageResource b3();
+	ImageResource b4();
+	ImageResource b5();
+	ImageResource b6();
+	ImageResource b7();
+	ImageResource b8();
+	ImageResource flagBlue();
+	ImageResource flagRed();
+	ImageResource unknown();
+}
