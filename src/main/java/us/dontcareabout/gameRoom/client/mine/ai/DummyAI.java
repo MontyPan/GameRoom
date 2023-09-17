ee2283d6e654d40198f7b0d027f1dd2bff5dc78a
MontyPan <monty.pan@dontcareabout.us>
2023-09-17 11:12:34 +0800

    refactor：抽出 AiUtil

--- src/main/java/us/dontcareabout/gameRoom/client/mine/ai/DummyAI.java
+++ src/main/java/us/dontcareabout/gameRoom/client/mine/ai/DummyAI.java
@@ -1,24 +1,14 @@
 package us.dontcareabout.gameRoom.client.mine.ai;
 
-import java.util.Random;
-
-import us.dontcareabout.gameRoom.client.mine.MineGM;
 import us.dontcareabout.gameRoom.client.mine.Player;
 import us.dontcareabout.gameRoom.client.mine.vo.GameInfo;
 import us.dontcareabout.gameRoom.client.mine.vo.XY;
 
 public class DummyAI implements Player {
-	private Random random = new Random();
 
 	@Override
 	public XY guess(GameInfo info) {
-		int x, y;
-		do {
-			x = random.nextInt(info.getWidth());
-			y = random.nextInt(info.getHeight());
-		} while(info.getMap()[x][y] != MineGM.UNKNOW);
-
-		return new XY(x, y);
+		return AiUtil.blindGuess(info);
 	}
 
 	@Override
