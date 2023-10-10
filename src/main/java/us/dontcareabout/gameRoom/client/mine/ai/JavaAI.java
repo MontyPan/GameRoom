39d9b9f4ee45eaf19ef63875911489706c20f8bd
MontyPan <monty.pan@dontcareabout.us>
2023-10-10 16:41:14 +0800

    refactor：JavaAI 改為單純的 AI 行為 interface，不再負責任何實做

--- src/main/java/us/dontcareabout/gameRoom/client/mine/ai/JavaAI.java
+++ src/main/java/us/dontcareabout/gameRoom/client/mine/ai/JavaAI.java
@@ -1,40 +1,9 @@
 package us.dontcareabout.gameRoom.client.mine.ai;
 
-import com.google.gwt.core.client.Scheduler;
-
-import us.dontcareabout.gameRoom.client.mine.GM;
-import us.dontcareabout.gameRoom.client.mine.Player;
 import us.dontcareabout.gameRoom.client.mine.vo.GameInfo;
+import us.dontcareabout.gameRoom.client.mine.vo.XY;
 
-public abstract class JavaAI implements Player {
-	public final int index;
-
-	public JavaAI(int index) {
-		this.index = index;
-		GM.addGameStart(e -> start(e.data));
-		GM.addGameMove(e -> move(e.data));
-		//game end 不用處理
-	}
-
-	protected void start(GameInfo data) {}
-
-	protected void move(GameInfo data) {
-		if (!data.isMyTurn(index)) { return; }
-
-		//用 fixed delay 是為了讓 UX 看起來 一點
-		//但因為 event flow 的關係，至少得弄個 scheduleDeferred()
-		Scheduler.get().scheduleFixedDelay(
-			() -> {
-				GM.move(index, guess(data));
-				return false;
-			},
-			500
-		);
-	}
-
-	@Override
-	public final int getIndex() { return index; }
-
-	@Override
-	public	final Type type() { return Type.Java; }
+public interface JavaAI {
+	public String name();
+	public XY guess(GameInfo info);
 }
