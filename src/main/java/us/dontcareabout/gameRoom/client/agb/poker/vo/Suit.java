a309f9ba6fc637b9e64bb29075059d894d4dfd0f
MontyPan <monty.pan@dontcareabout.us>
2023-11-06 17:29:58 +0800

    增加：Suit

---
+++ src/main/java/us/dontcareabout/gameRoom/client/agb/poker/vo/Suit.java
@@ -0,0 +1,14 @@
+package us.dontcareabout.gameRoom.client.agb.poker.vo;
+
+/**
+ * 撲克牌的花色。
+ * <p>
+ * 鬼牌的花色為 {@link #joker}。
+ */
+public enum Suit {
+	spade,
+	heart,
+	diamond,
+	club,
+	joker;
+}
